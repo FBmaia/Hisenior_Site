ad88eda6accc3fbce71d375016812a5d74db69fb
unknown <fabricio.maia.antonio1@gmail.com>
2021-07-16 17:09:29 -0300

    commit hisenior

---
+++ js/funcoes.js
@@ -0,0 +1,19 @@
+$("#slider").nivoSlider();
+
+
+
+$(".galeria").slick({
+
+    slidesToShow: 1,
+
+    slidesToScroll: 1,
+
+    autoplay: true,
+
+    autoplaySpeed: 2000,
+
+    fade: false,
+
+    cssEase: 'none'
+
+});
